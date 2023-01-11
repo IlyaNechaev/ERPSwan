a43ac39b09877920a8855f234169b8d6ace7392b
Ilya Nechaev <ilia.nechaeff@yandex.ru>
2023-01-11 22:27:35 +0300

    Добавить Route

--- Client/react-client/src/App.js
+++ Client/react-client/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
